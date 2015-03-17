c696ea47a6ff501bd04f0330adcec822d7bc72dd
Zhenya1996 <gurkov1972@mail.ru>
2015-03-17 17:20:17 +0300

    edit

--- chat/main.js
+++ chat/main.js
@@ -1,15 +1,37 @@
-$(document).ready(function () {
-    var addMessage = function () {
-        var msg = $('textarea[name="msg-area"]').val();
-        var name = $('.chat-name').val();
-        $('.chat-messages').append('<div class="msg-data">' + name + ' : ' + msg + '</div>');
-        $('textarea[name="msg-area"]').val('');
-    } 
-    $('.submit').click(addMessage);
-    $('textarea[name="msg-area"]').keypress(function(e){
-        if(e.which == 13){
-            e.preventDefault();
-            addMessage();
-        }
-    });
-});
+
+function run(){
+var container=document.getElementsByClassName('wrapper')[0];
+container.addEventListener('click',delegateEvent);
+container.addEventListener('change',delegateEvent);
+}
+
+function delegateEvent(eventObject){
+    if(eventObject.type==='click' && eventObject.target.classList.contains('submit')){
+        onSendButtonClick();
+    }
+  /*  if(eventObject.type==='change' && eventObject.target.parentElement.classList.contains('message')){
+     onCheckboxClick(event);
+    } //.classList.contains('submit')){*/
+    
+}
+/*function  onCheckboxClick(event){
+if(event.target.parentElement.classList.add('selected');
+}*/
+function onSendButtonClick(){
+    var textMessage=document.getElementById('msg-input');
+    if(!textMessage)
+        return;
+    var name = document.getElementsByClassName('chat-name')[0];
+    var message = document.createElement('div');
+    message.setAttribute('class','message');
+    var history = document.getElementsByClassName('chat-messages')[0];
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute('type','checkbox');
+    message.appendChild(checkbox);
+    message.appendChild(document.createTextNode(name.value+": "));
+    message.appendChild(document.createTextNode(textMessage.value+" "));
+    textMessage.value='';
+    history.appendChild(message);
+}
+
+
